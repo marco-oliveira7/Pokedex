1a7e68329e3b21aa886c25d69a7b539e0396ca99
Marco <ma8155354@gmail.com>
2024-11-14 15:44:10 -0300

    Infos pokemons add
    Novas informações dos pokemons adicionadas e ajustes no css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,25 @@
 
-const offset = 0
-const limit = 10
-const urlPokedex = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToHtml(pokemon) {
     return `
-        <li class="pokemon">
-                <span id="pokemonNumber">#001</span>
-                <span class="name">${pokemon.name}</span>
-                <div class="detail">
-                    <ol class="types">
-                        <li class="type">Grass</li>
-                        <li class="type">Poison</li>
-                    </ol>
-
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-                        alt="${pokemon.name}">
-                </div>
-            </li>
-        `
+    <li class="pokemon ${pokemon.type}">
+        <span id="pokemonNumber">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                
+            <img src="${pokemon.photo}"
+            alt="${pokemon.name}">
+        </div>
+    </li>
+`
 }
 
 const pokemonList = document.getElementById('pokemonList')
+pokeApi.getPokemons().then((pokemons = []) => {
 
-fetch(urlPokedex)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-
-        pokemons.forEach(pokemon => {
-
-            pokemonList.innerHTML += convertPokemonToHtml(pokemon)
-
-        });
-    })
-    .catch((error) => console.error(error))
+    // Utilizando o metodo map exemplificado
+    pokemonList.innerHTML = pokemons.map(convertPokemonToHtml).join('')
+})
